2e435b60e22bc9fc5e3431f79b5aa17424f1a2d4
Nik Bailey <bail0301@morris.umn.edu>
2023-04-03 21:43:07 -0500

    Added code to all tasks

--- Ruby01PPL.rb
+++ Ruby01PPL.rb
@@ -1,7 +1,7 @@
 ## Task 1
 $hm = Hash.new
 
-str  = 'This is a string and A STRING THIS IS'
+str  = 'This is a string? A STRING THIS IS!!!!'
 lowerstr = str.downcase
 
 lowerstr.each_char { |c| 
@@ -22,3 +22,48 @@ puts "The most frequent character in the string is #{biggestChar}"
 
 ## Task 3
 
+def useLambda(hash, lambda)
+    hash.inject(0) {|sum, (key, val)| lambda.call(sum, key, val)}
+end 
+
+def isVowel(c)
+    return !c.downcase.match(/[aeiou]/).nil?
+end
+
+numVowels = -> (sum, key, val) {
+    sum ||= 0 # set to 0 if nil, same for the ones below
+    if isVowel(key)
+        sum + val
+    else
+        sum
+    end
+}
+
+numSpeChars = -> (sum, key, val) {
+    sum ||= 0
+    is_non_letter = key.match(/[^a-zA-Z]/)
+    if is_non_letter
+        sum + val
+    end
+}
+
+onlyOnce = -> (sum, key, val) {
+    sum ||= 0
+    if val == 1
+        sum + 1
+    else
+        sum
+    end
+}
+
+# Test case for numVowels lambda
+result = useLambda($hm, numVowels)
+puts "Total number of vowels in the hash: #{result}"
+
+# Test case for numNonLetters lambda
+result = useLambda($hm, numSpeChars)
+puts "Total number of non-letter characters in the hash: #{result}" 
+
+# Test case for numOnce lambda
+result = useLambda($hm, onlyOnce)
+puts "Total number of keys with a value of 1 in the hash: #{result}"
